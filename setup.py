62cc6392e8f15343186963332a3c4b32f9bb8fc5
Sparisoma Viridi <dudung@gmail.com>
2024-06-19 22:12:26 +0700

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='butiranx',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=[
         # List your project dependencies here
